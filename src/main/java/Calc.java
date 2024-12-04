2e99eaa8dbfadc4886c519c70698b310e7a03ae8
iaminwoo <hwang-in-woo@hwang-in-uui-MacBookAir.local>
2024-12-04 16:57:15 +0900

    단순 뺄셈 완성

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,9 +1,25 @@
 public class Calc {
     public static int run(String input) {
-        String[] inputParts = input.split("\\+");
-        int firstNum = Integer.parseInt(inputParts[0].strip());
-        int secondNum = Integer.parseInt(inputParts[1].strip());
-
-        return firstNum + secondNum;
+        input = input.replace(" ","");
+        if(input.contains("+")){
+            return operation(input, "\\+");
+        } else if (input.contains("-")) {
+            return operation(input, "-");
+        }
+        return 0;
     }
+
+    public static int operation(String input, String operator) {
+        String[] inputParts = input.split(operator);
+
+        int firstNum = Integer.parseInt(inputParts[0]);
+        int secondNum = Integer.parseInt(inputParts[1]);
+
+        if(operator.equals("\\+")) return firstNum+secondNum;
+        if(operator.equals("-")) return firstNum-secondNum;
+
+        return 0;
+    }
+
+
 }
