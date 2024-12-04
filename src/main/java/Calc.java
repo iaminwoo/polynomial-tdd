8ec8effa3c143580bc60a129f14396d217162f50
iaminwoo <hwang-in-woo@hwang-in-uui-MacBookAir.local>
2024-12-04 18:35:16 +0900

    단순 곱셈 추가

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,8 +1,30 @@
 public class Calc {
     public static int run(String input) {
-        return simplePlusMinus(input);
+        int result;
+
+        if(input.contains("*")){
+            result = multiply(input);
+        }else{
+            result = simplePlusMinus(input);
+        }
+
+        return result;
     }
 
+    private static int multiply(String input) {
+        input = input.replace(" ","");
+
+        String[] parts = input.split("\\*");
+
+        int first = Integer.parseInt(parts[0]);
+        int sec = Integer.parseInt(parts[1]);
+
+
+        return (first * sec);
+    }
+
+    // 각 부호별로 분리해서 더함
+    // 예를들어 "10 - 10 - 10" -> [10, -10, -10]으로
     public static int simplePlusMinus(String input) {
         input = input.replace(" ","");
 
